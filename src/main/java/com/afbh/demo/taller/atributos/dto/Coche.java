b430a634fe8754e6e771edc246fe7834ba244d18
Felipe-hen30 <henao-30@outlook.com>
2025-05-21 23:55:54 +0200

    guardando tercer y cuarto atributo

---
+++ src/main/java/com/afbh/demo/taller/atributos/dto/Coche.java
@@ -0,0 +1,9 @@
+package com.afbh.demo.taller.atributos.dto;
+
+public class Coche {
+	
+	private String marca ="porsche";
+	private int ruedas = 8;
+	private double motor = 8.8;
+
+}
---
+++ src/main/java/com/afbh/demo/taller/atributos/dto/Leon.java
@@ -0,0 +1,9 @@
+package com.afbh.demo.taller.atributos.dto;
+
+public class Leon {
+
+	private String raza = "neolandes";
+	private int numeroDeRayas = 27;
+	private double medidaGarras = 5.5;
+
+}

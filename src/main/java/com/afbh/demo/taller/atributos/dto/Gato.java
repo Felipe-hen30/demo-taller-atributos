97d92722fd056c2a3cbd84a91a3faf66b31a0602
Felipe-hen30 <henao-30@outlook.com>
2025-05-21 23:42:47 +0200

    guardando las primeras clases

---
+++ src/main/java/com/afbh/demo/taller/atributos/dto/Gato.java
@@ -0,0 +1,9 @@
+package com.afbh.demo.taller.atributos.dto;
+
+public class Gato {
+
+	private String enfermedad = "leucemia";
+	private int esperanzaVida = 4;
+	private double anchura = 3.4;
+
+}
---
+++ src/main/java/com/afbh/demo/taller/atributos/dto/Perro.java
@@ -0,0 +1,9 @@
+package com.afbh.demo.taller.atributos.dto;
+
+public class Perro {
+
+	private String nombre = "Thor";
+	private int numeroPatas = 4;
+	private double altura = 0.89;
+
+}
